6e7c04063b01deb56f5de8b3989d14b160a68d9a
Rasmus Rask <rasse.rask@gmail.com>
2024-10-18 09:53:49 +0300

    feat(UI): Add rectangle that spawns on right click when player presses right click in window and vanishes when you press left click.

---
+++ includes/UI.hpp
@@ -0,0 +1,19 @@
+#ifndef UI_HPP
+#define UI_HPP
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class UI {
+	public:
+		UI();
+		~UI();
+		sf::Text			getText() const;
+		void				setText(std::string text);
+	private:
+		sf::Text 		_text;
+		sf::Font		_font;
+		sf::Vector2i	_position;
+};
+
+#endif
---
+++ src/UI.cpp
@@ -0,0 +1,25 @@
+#include "../includes/UI.hpp"
+
+UI::UI() {
+	if (!_font.loadFromFile("fonts/Roboto-Medium.ttf")) {
+		std::cerr << "Failed to load font!" << std::endl;
+	}
+	_text.setFont(_font);
+	_text.setString("Hello, World!");
+	_text.setCharacterSize(24);
+	_text.setFillColor(sf::Color::White);
+	_text.setStyle(sf::Text::Bold);
+	_text.setPosition({ 10, 10 });
+}
+
+UI::~UI() {
+	std::cout << "UI destroyed!" << std::endl;
+}
+
+sf::Text UI::getText() const {
+	return _text;
+}
+
+void UI::setText(std::string text) {
+	_text = sf::Text(text, _font, 24);
+}
